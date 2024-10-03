de34a9856c45e3d89056e67f3ba9cc4e64e355b5
Alain Boudard <alain.boudard@soprasteria.com>
2024-10-03 09:18:33 +0200

    fix: first components

---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,17 @@
+// list of Todo items
+
+import React from "react";
+import Todo from "../../todo/Todo";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/todo/Todo.js
@@ -0,0 +1,9 @@
+// react Todo component
+
+import React from "react";
+
+const Todo = () => {
+  return <div>This is the Todo component</div>;
+};
+
+export default Todo;
