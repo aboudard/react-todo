ca92b8bc2eedd40a6f5b3cb05c608d26c6041b8a
Alain Boudard <alain.boudard@soprasteria.com>
2024-10-22 10:45:11 +0200

    fix: fetch http and group headers

--- src/components/todo-list/TodoList.js
+++ src/components/todo-list/TodoList.js
@@ -3,19 +3,27 @@
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { InputSwitch } from "primereact/inputswitch";
 import React, { useContext, useEffect } from "react";
 import { TodosContext } from "../../contexts/todos.context";
-import { TodoService } from "../../services/todo.service";
-import { InputSwitch } from "primereact/inputswitch";
 
 const TodoList = () => {
+  const API_URL = process.env.REACT_APP_API_URL
   const [state, dispatch] = useContext(TodosContext);
+  const apiUrl = API_URL + '/todos';
 
   useEffect(() => {
-    TodoService.getTodos().then((data) =>
+    console.log("useEffect");
+    /* TodoService.getTodos().then((data) =>
       dispatch({ type: "SET_TODOS", payload: data })
-    );
-  }, []);
+    ); */
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: "SET_TODOS", payload: data });
+        console.log("data", data);
+      });
+  });
 
   const editTodo = (todo) => {
     dispatch({ type: "TOGGLE_EDIT_MODE", payload: todo });
@@ -24,6 +32,15 @@ const TodoList = () => {
   const setChecked = (todo) => {
     console.log("todo", todo);
     dispatch({ type: "SET_TODO_ACTIVE", payload: todo });
+  };
+
+  const headerTemplate = (data) => {
+    return (
+      <div className="flex align-items-center gap-2">
+        <i className="pi pi-android" style={{ fontSize: '2rem' }}></i>
+        <span className="font-bold">{data.category}</span>
+      </div>
+    );
   };
 
   const actionTemplate = (todo) => {
@@ -45,6 +62,14 @@ const TodoList = () => {
       <h1>Todo List - {state.isEditMode ? "edit" : "read"}</h1>
       <div className="card">
         <DataTable
+          rowGroupMode="subheader"
+          groupRowsBy="category"
+          sortMode="single"
+          sortField="category"
+          sortOrder={1}
+          scrollable
+          scrollHeight="400px"
+          rowGroupHeaderTemplate={headerTemplate}
           size="small"
           value={state.todos}
           tableStyle={{ minWidth: "50rem" }}
@@ -55,9 +80,7 @@ const TodoList = () => {
           <Column field="description" header="Description"></Column>
           <Column header="Actions" body={actionTemplate}></Column>
         </DataTable>
-        <div>
-          {JSON.stringify(state.todos)}
-        </div>
+        <div>{JSON.stringify(state.todos)}</div>
       </div>
     </div>
   );
